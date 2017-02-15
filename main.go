6a9cde2d9ee65fc1bdbcd553d4daa199493b5588
NaohiroKashimoto <n-kashimoto@ceres-inc.jp>
2017-02-15 13:42:18 +0900

    ValueCommerceAdded

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"fmt"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/sclevine/agouti"
-	"golang.org/x/text/encoding/japanese"
 )
 
 func main() {
@@ -48,25 +45,4 @@ func loginvc() {
 	}
 
 	page.Screenshot("test")
-}
-
-func goqueryexample() {
-
-	response, err := http.Get("http://1.dev.osaifu2.com")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	document, err := goquery.NewDocumentFromResponse(response)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	//ShiftJISの処理（ShiftJIS->UTF-8）
-	decoder := japanese.ShiftJIS.NewDecoder()
-	text, err := decoder.String(document.Find("html body#top div#container div#content.clearfix div.main-column div.section.feature.item-block ul.clearfix li a.new span.detail span.name").Text())
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	fmt.Println(text)
 }
